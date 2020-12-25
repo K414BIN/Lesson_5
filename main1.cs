7423091665e7bc596bd5130bc3f6a2fa4ae20efc
Alexander Kalabin <k414bin@yandex.ru>
2020-12-25 22:39:55 +0300

    Task 1 Added

---
+++ main1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace files
+{  
+    class Program 
+	{	
+    static void Main(string[] args)
+    {    
+	System.Console.WriteLine("Введите текстовое сообщение:");
+	string filename = "readme.txt";
+    string str1 = System.Console.ReadLine();
+
+	File.WriteAllText(filename, str1);
+	//
+	Console.ReadLine();
+  
+	}
+	}
+}
