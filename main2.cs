824ae7a69643b54e3c6857e532f3061cbee33fc2
Alexander Kalabin <k414bin@yandex.ru>
2020-12-25 15:15:58 +0300

    Task 2 added

---
+++ main2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace files
+{  
+    class Program 
+	{	
+    static void Main(string[] args)
+    {    
+	string filename = "startup.txt";
+    string str1 = DateTime.Now.ToString();
+	File.AppendAllText(filename, Environment.NewLine); 
+	File.AppendAllText(filename, str1);
+	Console.WriteLine(DateTime.Now);
+	Console.ReadLine();
+  
+	}
+	}
+}
