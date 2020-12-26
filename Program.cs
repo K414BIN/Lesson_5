8a9d2726b76f7feb5ce92a8a8f9e0656d17019ca
Alexander Kalabin <k414bin@yandex.ru>
2020-12-26 19:39:56 +0300

    Task 3 Added

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lesson_5
 {
@@ -10,6 +11,27 @@ namespace Lesson_5
     {
         static void Main(string[] args)
         {
+
+            string fileName = "ByteFile.bin";
+            Console.WriteLine(" Введите через пробел целые положительные цифры от 1 до 255. ");
+            string text = Console.ReadLine();
+            byte[] byteArr = new byte[] { };
+            try
+            {
+                 byteArr = text.Split(' ').Select(x => Convert.ToByte(x)).ToArray();
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Введено значение больше 255!");
+            }
+            finally
+            {
+                File.WriteAllBytes(fileName, byteArr);
+            }
+            Console.ReadLine();
+
         }
     }
 }
