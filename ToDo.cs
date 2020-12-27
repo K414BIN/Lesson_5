e0e0e80e48a106dd39fd9d828f23713706d5c5ba
Alexander Kalabin <74727055+K414BIN@users.noreply.github.com>
2020-12-27 23:33:27 +0300

    Add files via upload
    (*) Список задач (ToDo-list):
    написать приложение для ввода списка задач;
    задачу описать классом ToDo с полями Title и IsDone;
    это не сделано - на старте, если есть файл tasks.json/xml/bin (выбрать формат), загрузить из него массив имеющихся задач и вывести их на экран;
    если задача выполнена, вывести перед её названием строку «[x]»;
    это не сделано - вывести порядковый номер для каждой задачи;
    это не сделано - при вводе пользователем порядкового номера задачи отметить задачу с этим порядковым номером как выполненную;
    записать актуальный массив задач в файл tasks.json/xml/bin.

---
+++ ToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    public class ToDo
+    {
+        public string Title { get; set; }
+        public bool isDone { get; set; }
+        public void duty( string str, bool done)
+        {
+          Title = str ;
+          isDone = done;
+        }
+
+
+    }
+}
